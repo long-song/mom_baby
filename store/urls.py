7dc5a2b7d36c47fc0b0d07a17df41ed41c6513e8
long-song <1179650637@qq.com>
2019-07-09 20:37:23 +0800

    增加了各个页面的路由,并对HTML页面路由进行修改

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from store import views
 
 urlpatterns = [
-    path('index/', views.index),   # 访问首页路由
-    path('login/', views.login),   # 访问登录路由
+    path('index/', views.index , name='index'),   # 访问首页路由
+    path('login/', views.login, name='login'),   # 访问登录路由
+    path('list/', views.list, name='list'),   # 访问商品列表路由
+    path('buytoday/', views.buytoday, name='buytoday'),   # 访问今日团购路由
+    path('information/', views.information, name='information'),   # 访问母婴资讯路由
+    path('about/', views.about, name='about'),   # 访问关于我们路由
+    path('shopcart/', views.shopcart, name='shopcart'),   # 访问购物车路由
+    path('details/', views.details, name='details'),   # 访问产品详情路由
 ]
--- store/views.py
+++ store/views.py
@@ -17,3 +17,51 @@ def login(request):
     :return:
     '''
     return render(request,'login.html')
+
+def list(request):
+    '''
+    访问商品列表页面
+    :param request:
+    :return:
+    '''
+    return render(request,'commodity.html')
+
+def buytoday(request):
+    '''
+    访问今日团购页面
+    :param request:
+    :return:
+    '''
+    return render(request,'buytoday.html')
+
+def information(request):
+    '''
+    访问母婴资讯页面
+    :param request:
+    :return:
+    '''
+    return render(request,'information.html')
+
+def shopcart(request):
+    '''
+    访问购物车页面
+    :param request:
+    :return:
+    '''
+    return render(request,'shopcart.html')
+
+def about(request):
+    '''
+    访问关于我们页面
+    :param request:
+    :return:
+    '''
+    return render(request,'about.html')
+
+def details(request):
+    '''
+    访问产品详情页面
+    :param request:
+    :return:
+    '''
+    return render(request,'details.html')
